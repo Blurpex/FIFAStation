e9808d4c9d9c86f8c583d8766460f599d255be96
haris <harizahmad100@gmail.com>
2023-04-15 19:09:00 -0400

    initial player query layout

--- app/src/main/java/com/example/fifastation/PlayerActivity.java
+++ app/src/main/java/com/example/fifastation/PlayerActivity.java
@@ -1,13 +1,8 @@
 package com.example.fifastation;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.squareup.picasso.Picasso;
-
-import java.io.InputStream;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -15,9 +10,6 @@ public class PlayerActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-
-        ImageView image = findViewById(R.id.imageView);
-        Picasso.get().load("https://cdn.sofifa.net/players/158/023/22_120.png").placeholder(R.mipmap.ic_launcher).into(image);
     }
 
 }
