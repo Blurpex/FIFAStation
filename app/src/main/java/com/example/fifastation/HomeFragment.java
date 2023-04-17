91752e0c9df60fc4d059dd966aea2ff071497e2c
Haris Ahmad <harizahmad100@gmail.com>
2023-04-17 18:09:38 -0400

    restructured to use navigation UI

---
+++ app/src/main/java/com/example/fifastation/HomeFragment.java
@@ -0,0 +1,81 @@
+package com.example.fifastation;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fifastation.db.PlayerDatabase;
+
+public class HomeFragment extends Fragment {
+
+    private Context context;
+    private RecyclerView recycler;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // get the layout
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        // content for the activity
+        this.context = container.getContext();
+        this.recycler = view.findViewById(R.id.trending_player_list);
+        displayTrendingPlayers();
+
+        // inflate the layout
+        return view;
+    }
+
+    private void displayTrendingPlayers() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        PlayerDatabase.getInstance(context);
+        PlayerDatabase.getTopTenRatedPlayers(players -> {
+            PlayerAdapter adapter = new PlayerAdapter(context, players);
+            recycler.setAdapter(adapter);
+            recycler.setLayoutManager(layoutManager);
+        });
+    }
+
+    /*
+    private void displayCopyrightDialog() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        boolean isFirstRun = sharedPreferences.getBoolean("IS_FIRST_RUN", true);
+
+        if(isFirstRun) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage("The data used on this app is from website and not intended to use copyright.");
+            alertDialog.setIcon(R.drawable.ic_alert);
+            alertDialog.setPositiveButton("Acknowledge",
+                    (dialog, which) -> Toast.makeText(MainActivity.this, "Thanks for your acknowledge!!!", Toast.LENGTH_LONG).show());
+            alertDialog.show();
+
+            // Update
+            editor.putBoolean("IS_FIRST_RUN", false);
+            editor.commit();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // Update
+        editor.putBoolean("IS_FIRST_RUN", true);
+        editor.commit();
+    }
+    */
+
+}
--- app/src/main/java/com/example/fifastation/MainActivity.java
+++ app/src/main/java/com/example/fifastation/MainActivity.java
@@ -1,20 +1,12 @@
 package com.example.fifastation;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.example.fifastation.db.PlayerDatabase;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -42,76 +34,21 @@ public class MainActivity extends AppCompatActivity
         // drawer
         this.drawer = findViewById(R.id.drawer);
         this.toolbar.setNavigationOnClickListener(view -> drawer.open());
-
-        // content for the activity
-        displayCopyrightDialog();
-        displayTrendingPlayers();
     }
 
-    // handles navigation item when selected
+    // handles navigation item when drawer item is selected
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
         if(itemId == R.id.players) {
-            test("players: " + R.id.players);
-            Intent intent = new Intent(this, PlayerActivity.class);
-            startActivity(intent);
+//            Intent intent = new Intent(this, PlayerActivity.class);
+//            startActivity(intent);
         }
         else if(itemId == R.id.favorite) {
-            test("favorite: " + R.id.favorite);
         }
         else if(itemId == R.id.settings) {
-            test("settings: "  + R.id.settings);
         }
         this.drawer.close();
         return true;
-    }
-
-    private void displayTrendingPlayers() {
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        RecyclerView recyclerView = findViewById(R.id.trending_player_list);
-        PlayerDatabase.getInstance(this);
-        PlayerDatabase.getTopTenRatedPlayers(players -> {
-            PlayerAdapter adapter = new PlayerAdapter(this, players);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setLayoutManager(layoutManager);
-        });
-    }
-
-    private void displayCopyrightDialog() {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        boolean isFirstRun = sharedPreferences.getBoolean("IS_FIRST_RUN", true);
-
-        if(isFirstRun) {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-            alertDialog.setTitle("Alert");
-            alertDialog.setMessage("The data used on this app is from website and not intended to use copyright.");
-            alertDialog.setIcon(R.drawable.ic_alert);
-            alertDialog.setPositiveButton("Acknowledge",
-                    (dialog, which) -> Toast.makeText(MainActivity.this, "Thanks for your acknowledge!!!", Toast.LENGTH_LONG).show());
-            alertDialog.show();
-
-            // Update
-            editor.putBoolean("IS_FIRST_RUN", false);
-            editor.commit();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        // Update
-        editor.putBoolean("IS_FIRST_RUN", true);
-        editor.commit();
-    }
-
-    private void test(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
