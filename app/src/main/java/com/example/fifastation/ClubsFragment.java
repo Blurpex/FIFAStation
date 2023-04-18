3a7a8432d8ea72feb4ae4fe1fd8022e319218706
Haris Ahmad <harizahmad100@gmail.com>
2023-04-17 20:24:50 -0400

    added files for each navigation

---
+++ app/src/main/java/com/example/fifastation/ClubsFragment.java
@@ -0,0 +1,18 @@
+package com.example.fifastation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ClubsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_clubs, container, false);
+    }
+}
