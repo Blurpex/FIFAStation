52d02701aca18e0a707cb31a06a8cba040dccc0c
SugaWrld <nhkumcaleb@gmail.com>
2023-04-26 00:52:01 -0400

    created layout for settings

---
+++ app/src/main/java/com/example/fifastation/SettingsFragment.java
@@ -0,0 +1,20 @@
+package com.example.fifastation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // get the layout
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // inflate layout
+        return view;
+    }
+}
