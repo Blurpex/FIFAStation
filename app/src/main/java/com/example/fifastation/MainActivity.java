97d5b099af87dfce8c2399ffdfc168de6d03ea25
Haris Ahmad <harizahmad100@gmail.com>
2023-04-27 16:35:55 -0400

    fixed navigation

--- app/src/main/java/com/example/fifastation/MainActivity.java
+++ app/src/main/java/com/example/fifastation/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.fifastation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +40,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // top app bar
+        // ack dialog
+        displayCopyrightDialog();
+
+        // setup toolbar
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
 
-        // drawer
+        // setup navigation
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
         DrawerLayout drawer = findViewById(R.id.drawer);
-        toolbar.setNavigationOnClickListener(view -> drawer.open());
-
-        // navigation
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setDrawerLayout(drawer).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationView navigation = findViewById(R.id.navigation);
-        NavController controller = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navigation, controller);
-        controller.addOnDestinationChangedListener((navController, navDestination, bundle) -> {
-            toolbar.setTitle(navDestination.getLabel());
-        });
+        NavigationUI.setupWithNavController(navigation, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    // ask user to ack
+    private void displayCopyrightDialog() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // show the dialog only once
+        if(!sharedPreferences.getBoolean("dataAck", false)) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+            alertDialog.setTitle("Terms & Conditions");
+            alertDialog.setMessage("This app is not for commercial use and not intended to use copyright.");
+            alertDialog.setIcon(R.drawable.ic_alert);
+            alertDialog.setPositiveButton("Acknowledge", (dialog, which) ->
+                    Toast.makeText(MainActivity.this, "Thank you for acknowledging!", Toast.LENGTH_LONG).show());
+            alertDialog.show();
+
+            // save it to shared preferences
+            editor.putBoolean("dataAck", true);
+            editor.apply();
+        }
     }
 }
